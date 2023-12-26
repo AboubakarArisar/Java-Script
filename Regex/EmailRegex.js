0ba051f398a9e2377624a45a4c7b3331f8e8f1bf
Aboubakar <116747262+AboubakarArisar@users.noreply.github.com>
2023-12-26 22:48:49 +0500

    email regex in javascript

---
+++ Regex/EmailRegex.js
@@ -0,0 +1,9 @@
+                //Email Regex
+
+const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+let email = "pindaricoders@gmail.com";
+
+console.log(emailRegex.test(email)); //it returns true
+
+email = "pindaricoders@email";
+console.log(emailRegex.test(email)); //it returns false
